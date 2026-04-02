6e4a8b9434a2b3ecaeea7c224056c5cf628821e0
ChristianDiaz12 <90482847+ChristianDiaz12@users.noreply.github.com>
2026-04-02 13:29:55 -0400

    Create dishes.router.js

---
+++ src/dishes/dishes.router.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const controller = require("./dishes.controller"); 
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+// Routers for /dishes
+router
+.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed); 
+
+// Routers for /dishes/:dishId
+router
+.route("/:dishId")
+.get(controller.read)
+.put(controller.update)
+.all(methodNotAllowed);
+
+module.exports = router;
